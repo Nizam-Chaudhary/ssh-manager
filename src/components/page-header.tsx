b9d66e6d06c1f1e06c998dddab7054d9343ce4c0
Nizam Chaudhary <ChaudharyNizam10@gmail.com>
2026-03-04 23:12:27 +0530

    fix: header separator

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -9,8 +9,10 @@ interface PageHeaderProps {
 export function PageHeader({ title, children }: PageHeaderProps) {
     return (
         <header className='flex h-14 shrink-0 items-center gap-2 border-b px-4'>
-            <SidebarTrigger className='-ml-1' />
-            <Separator orientation='vertical' className='mr-2 h-4!' />
+            <SidebarTrigger />
+            <div>
+                <Separator orientation='vertical' className='me-2 h-8' />
+            </div>
             <h1 className='text-lg font-semibold'>{title}</h1>
             <div className='ml-auto flex items-center gap-2'>{children}</div>
         </header>
